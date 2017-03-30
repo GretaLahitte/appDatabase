7f26dd8b0de4e09cd73b9983177817a65f12fd57
st3ffane <steph.ponteins@gmail.com>
2017-03-30 19:18:24 +0200

    test branch

---
+++ steff/core.js
@@ -0,0 +1,16 @@
+/**
+ * Les datas et controllers pour l'application
+ */
+var myContext = (function (){
+
+
+
+
+    return {
+        app_name : "GRETA SQL Tool",
+        app_slogan : "a sql tool that is super good!",
+
+    };
+})();
+
+console.log(myContext)
