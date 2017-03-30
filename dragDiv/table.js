48bffa593a4c2f51edf2fa4e266aa3c7feb59976
Greta Lahitte <greta.lahitte@outlook.fr>
2017-03-30 15:42:21 +0200

    mouse move

--- dragDiv/table.js
+++ dragDiv/table.js
@@ -18,14 +18,15 @@ function Table(head){
 		div.style.height = "100px";
 		div.style.background = "red";
 		div.style.color = "blue";
+		div.textContent=this.head;
 		div.style.textContent=this.head;
 		document.body.appendChild(div);
 		this.take(head)
 	};
 	this.take=function(_id){
 		console.log('take: ', _id);
-		divId=document.getElementById(_id);
-		divId.addEventListener('mousedown',this.drag,true);
+		div=document.getElementById(_id);
+		div.addEventListener('mousedown',this.drag,true);
 	};
 	this.drag=function(e){
 		isDown = true;
@@ -34,14 +35,14 @@ function Table(head){
 			this.offsetTop - e.clientY
 		];
 		console.log('drag');
-		divId.addEventListener('mousemove',function(event){
-			divId.addEventListener('mouseup', function() {
+		document.addEventListener('mousemove',function(event){
+			document.addEventListener('mouseup', function() {
 							event.preventDefault();
-
 				isDown = false;
-				console.log('mouseup');
-				this.style.zIndex=0;
+				//console.log('mouseup');
+				div.style.zIndex=0;
 			}, true);
+			//console.log('move event: ',event);
 			//console.log(this);
 			event.preventDefault();
 			if (isDown) {
@@ -51,9 +52,11 @@ function Table(head){
 					y : event.clientY
 		
 				};
-				this.style.left = (mousePosition.x + offset[0]) + 'px';
-				this.style.top  = (mousePosition.y + offset[1]) + 'px';
-				this.style.zIndex=1000;
+				var divid = document.getElementById(event.target.id);
+				//console.log(divid)
+				divid.style.left = (mousePosition.x + offset[0]) + 'px';
+				divid.style.top  = (mousePosition.y + offset[1]) + 'px';
+				divid.style.zIndex=1000;
 			}
 		}),true	
 	};
