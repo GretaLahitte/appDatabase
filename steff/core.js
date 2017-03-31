fb4af52a27c4598f5b24ceac2e3344854f3569ac
st3ffane <steph.ponteins@gmail.com>
2017-03-31 07:42:26 +0200

    first dt binding

--- steff/core.js
+++ steff/core.js
@@ -3,12 +3,33 @@
  */
 var myContext = (function (){
 
+    //Creation d'un jeu de données pour tester les renders
+    var db = {
+        //qqs infos d'ordre generales sur la base elle meme et l'utilisateur
+        file_url:'a/path',//chemin vers le fichier sql/dump ou enregistrer
+        db_name:"nom_de_la_base",
+        db_type:"postgres",//ou mysql, mongo....
+        db_port:5432,
+        host:"host",
+        login:"loginUtilisateur",
+        passwrd:"passwordUtilisateur",
+
+        //description des tables 
+        tables:[
+
+        ],
+        //desciption des relations entre les tables
+        relations:[
+
+        ]
+    }
 
 
 
     return {
         app_name : "GRETA SQL Tool",
-        app_slogan : "a sql tool that is super good!",
+        app_slogan : "a sql tool that is super cool!",
+        db:db
 
     };
 })();
