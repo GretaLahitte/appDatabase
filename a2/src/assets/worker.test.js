c172a4cd0c3822c5fdab87e1296a02e200cddbef
LAHITTE <greta.lahitte@outlook.fr>
2017-05-10 16:00:11 +0200

    mise en page

--- a2/src/assets/worker.test.js
+++ a2/src/assets/worker.test.js
@@ -1,11 +1,9 @@
 var CAN_HAVE_EXTRAS = ["character varying","character","bit","bit varying","numeric"];
 
 onmessage = function(evt){
-    var base = JSON.parse(evt.data);
+  var base = JSON.parse(evt.data);
 	var tab=[]; //pour stocker les commandes SQL 
 	base.indexes=[];
-
-	console.log(base);
 
 	//enumerations
 	for (var en in base.enumerations) tab.push(...createSqlEnum(en, base.enumerations[en]));
@@ -17,33 +15,24 @@ onmessage = function(evt){
 			tab.push('/*' ,table.comment, '*/\n');
 		}
 		tab.push('CREATE TABLE ',key,' (\n');
-		
 		for (champs in table.fields){
 			var cto = createSqlField(champs,table.fields[champs],key,base);
 			if(cto.length > 0) tab.push('   ',...cto);
-			
 		}
 		tab.pop();
 		tab.push('\n);\n\n');
-		
-		
 	}
 	tab.push('\n\n');
-    //les relations
-    for(var relation of base.relations){
-       //console.log(relation);
-        tab.push(...createSqlRelation(relation, base));
-    }
-
+  //les relations
+  for(var relation of base.relations){
+    tab.push(...createSqlRelation(relation, base));
+  }
 	tab.push('\n\n');
 	//les index
-    tab.push(...base.indexes);
-    
-    
-    tab.push("\n/*Generated with Greta SQLTool, because sometimes, I look at the stars and go like 'Fuck it!'*/\n");
-   // postMessage("some SQL datas....");
-    postMessage(tab.join(''));
-
+  tab.push(...base.indexes);
+  tab.push("\n/*Generated with Greta SQLTool, because sometimes, I look at the stars and go like 'Fuck it!'*/\n");
+  // postMessage("some SQL datas....");
+  postMessage(tab.join(''));
 }
 
 /**
@@ -80,20 +69,19 @@ function createSqlField(champs, field_desc,tableName,  base){
 	var special=[];
 	var indexes=base.indexes;
 	
-	
 	if(field_desc.type=='Composite'){
 		indexes.push('CREATE ');
 		if(field_desc.unique){
 			indexes.push(' UNIQUE');
 		}
 		indexes.push(' INDEX ',champs,' ON ',tableName,' (');
-		for (var val of field_desc.fields){  //usage de of au lieu de in!
+		for (var val of field_desc.fields){  //usage de of au lieu de in, retourne la valeur et pas le nom de la propriété!
 			indexes.push(val,', ');
 		}
 		indexes.pop();
 		indexes.push(');\n');
-		return tab
 		
+    return tab
 	}
 	
 	if(CAN_HAVE_EXTRAS.indexOf(field_desc.type)>= 0 && field_desc.type_extras && field_desc.type !=="Composite"){
@@ -102,7 +90,7 @@ function createSqlField(champs, field_desc,tableName,  base){
 
 		if(extra.length){
 			type.push(extra.length);
-		} else if(extra.precision){
+		}else if(extra.precision){
 			//et le reste...
 			type.push(""+extra.precision);
 			if(extra.scale) type.push(",",extra.scale);
@@ -110,33 +98,32 @@ function createSqlField(champs, field_desc,tableName,  base){
 		type.push(')');
 		
 	}
-
-
+  
 	if(field_desc.check){
 		check='CHECK ('+field_desc.check+')';
 	}
-	
 	if(field_desc.index){
-//		special.push(' INDEX');
-		base.indexes.push('CREATE INDEX ',champs,'_',tableName,' ON ',tableName,'(',champs,');\n');
-	}
-	
+  	base.indexes.push('CREATE INDEX ',champs,'_',tableName,' ON ',tableName,'(',champs,');\n');
+	}	
 	if(field_desc.not_null){
 		special.push(' NOT NULL');
-	}
-	if(field_desc.primary_key){
+	}	
+  if(field_desc.primary_key){
 		special.push(' PRIMARY KEY');
-	}
-	if(field_desc.unique){
+	}	
+  if(field_desc.unique){
 		special.push(' UNIQUE');
 	}
-	tab.push(champs,'    ',...type,' ',...special,' ',check);
-	if(field_desc.comment){
+	
+  tab.push(champs,'    ',...type,' ',...special,' ',check);
+	
+  if(field_desc.comment){
 		tab.push('	/* ',field_desc.comment,' */');
 	}
 	
 	tab.push(",\n");
-	return tab;
+	
+  return tab;
 }
 
 /**
@@ -147,9 +134,7 @@ function createSqlField(champs, field_desc,tableName,  base){
  * @return string array: sql commands
  */
 function createSqlRelation(relation, base ){
-
     var tab=[];
-    
     var to_table = relation.to.table;
     var to_field = relation.to.field;
     var from_table = relation.from.table;
@@ -160,7 +145,6 @@ function createSqlRelation(relation, base ){
     var ff = ftable.fields[from_field];
     
     if(ff.fields){
-       
         //des composites, doit créer les champs dans la base cible
         var new_fields=[];
         for(var field of ff.fields){
@@ -173,15 +157,12 @@ function createSqlRelation(relation, base ){
         tab.push("ALTER TABLE ",to_table," ADD FOREIGN KEY (");
         for(var nf of new_fields) tab.push(nf,",");
         tab.pop();
-
         tab.push(") REFERENCES ",from_table,"(");
         for(var nf of new_fields) tab.push(nf,",");
         tab.pop();
-
         tab.push(");\n");
 
     //des types simples...
-    } else tab.push ("ALTER TABLE ",to_table," ADD FOREIGN KEY (",to_field,") REFERENCES ",from_table,"(",from_field,");\n");
+    }else tab.push ("ALTER TABLE ",to_table," ADD FOREIGN KEY (",to_field,") REFERENCES ",from_table,"(",from_field,");\n");
     return tab;
 }
-
